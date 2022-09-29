9d34e2339cc2399fb02d99924a032ecf6dc8e985
Aasutosh <soniaasu2101@gmail.com>
2022-09-29 14:02:14 +0530

    initial files

---
+++ src/test/java/com/asutosh/ebs/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asutosh.ebs.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
